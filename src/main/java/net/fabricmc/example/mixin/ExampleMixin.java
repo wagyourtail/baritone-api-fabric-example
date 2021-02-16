396396657c9281a9483336a3c15a791b1d36a507
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-02-16 15:17:45 -0700

    it works!

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,5 +1,6 @@
 package net.fabricmc.example.mixin;
 
+import baritone.api.BaritoneAPI;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,5 +12,6 @@ public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		System.out.println("This line is printed by an example mod mixin!");
+		System.out.println(BaritoneAPI.getProvider().getPrimaryBaritone());
 	}
 }
